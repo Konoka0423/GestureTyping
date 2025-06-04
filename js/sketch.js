117dd157fbc096061855b9061fb261d69fbb64dc
SANO Konoka <usss0423@icloud.com>
2025-06-04 10:32:31 +0900

    速度変更

--- js/sketch.js
+++ js/sketch.js
@@ -87,7 +87,7 @@ function setup() {
 
       let now = millis();
       if (c === lastChar) {
-        if (now - lastCharTime > 700) {
+        if (now - lastCharTime > 850) {
           // 1秒以上cが同じ値である場合の処理
           typeChar(c);
           lastCharTime = now;
